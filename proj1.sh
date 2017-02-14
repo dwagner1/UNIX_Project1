c43105cc41c468ca8920a11611e00a44ea629927
John Doe <dylan@1vision.solutions>
2017-02-14 15:55:18 -0600

    Add function created

--- proj1.sh
+++ proj1.sh
@@ -1,5 +1,46 @@
 #! /bin/bash
 
+findRecord() {
+	echo "findRecord() not implemented..."
+}
+
+addRecord() {
+	#echo "addRecord() not implemented..."
+	clear
+
+	echo "Enter name: "
+	read name
+
+	echo "Enter address: "
+	read address
+
+	echo "Enter phone: "
+	read phone
+
+	echo "Enter email: "
+	read email
+
+	echo "$name,$address,$phone,$email" >> database.txt
+
+	echo " --- Insert successful --- "
+}
+
+updateRecord() {
+	echo "updateRecord() not implemented..."
+
+}
+
+removeRecord() {
+	echo "removeRecord() not implemented..."
+
+}
+
+displayData() {
+	echo "displayData() not implemented..."
+
+}
+
+
 echo " "
 echo "Welcome to my contact database, please select the in the following menu: "
 echo ""
@@ -7,29 +48,37 @@ echo ""
 
 while :
 do
+echo ""
+echo "Select fromt the options below..."
 echo "(a) Find a record"
 echo "(b) Add a new record"
 echo "(c) Update a record"
 echo "(d) Remove a record"
-echo "(e) Quit"
+echo "(e) Display the database"
+echo "(f) Quit"
 echo "> Selection is : "
 read option
 
 case $option in
 	a)
-		echo "Find a record... Do something"
+		findRecord
 	;;
 	b)
-		echo "Add a new record... Do something"
+		addRecord
 	;;
 	c)
-		echo "Update an existing record... Do something"
+		updateRecord
 	;;
 	d)
-		echo "Remove a record... Do something"
+		removeRecord
 	;;
 	e)
+		displayData
+	;;
+	f)
 		echo "Stopping"
 		exit;;
+	*)
+		echo "Invalid choice. "
 esac
 done
